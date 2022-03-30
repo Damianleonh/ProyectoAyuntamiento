1e6b9c888ca3b7d49f22abfbcf7fc6e4f5cf26da
Damianleonh <60727523+Damianleonh@users.noreply.github.com>
2022-03-30 15:23:22 -0600

    j

--- resources/views/programa/create.blade.php
+++ resources/views/programa/create.blade.php
@@ -1,1 +1,10 @@
-FORMULARIO DE CREACION DE PROGRAMA
+@extends('layout.header')
+
+@section('title', 'Formulario')
+
+@section('contenido')
+<div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">Ingresa el nombre del proyecto</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Proyecto area sistemas">
+</div>
+@endsection
